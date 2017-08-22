1bbfab376d2ef943e188fda523239e37711a3795
Elektron2016 <malokosoft@gmail.com>
2017-08-22 22:32:42 +0300

    Delete byteordering.d

--- Release/byteordering.d
+++
@@ -1,9 +0,0 @@
-byteordering.d byteordering.o: .././byteordering.c .././byteordering.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h
-
-.././byteordering.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
